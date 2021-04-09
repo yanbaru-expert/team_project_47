77375fd3cd5d0e4a96a7e72a07a9b53bf799c3f8
KENTA MITANI <mitani.kenta88@gmail.com>
2021-04-09 23:18:23 +0900

    rails db:seed実行時のインポート処理コメントを修正

--- lib/import_csv.rb
+++ lib/import_csv.rb
@@ -20,9 +20,9 @@ class ImportCsv
     list = import('db/csv_data/text_data.csv')
     Text.destroy_all #データの初期化
 
-    puts "インポート処理を開始"
+    puts "テキスト教材インポート処理を開始"
     Text.create!(list)
-    puts "インポート完了!!"
+    puts "テキスト教材のCSVデータインポート完了!!"
   end
 
   def self.movie_data
@@ -30,8 +30,8 @@ class ImportCsv
     list = import('db/csv_data/movie_data.csv')
     Movie.destroy_all #データの初期化
 
-    puts "インポート処理を開始"
+    puts "動画教材のCSVデータインポート処理を開始"
     Movie.create!(list)
-    puts "インポート完了!!"
+    puts "動画教材のCSVデータインポート完了!!"
   end
 end
