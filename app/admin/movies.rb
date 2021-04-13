2fe910c7d44ca6de298960260eab5920835b3449
KENTA MITANI <mitani.kenta88@gmail.com>
2021-04-13 19:03:00 +0900

    db/seeds.rbに管理者データ作成を実装

--- app/admin/movies.rb
+++ app/admin/movies.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Movie do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :genre, :title, :url
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:genre, :title, :url]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :genre, :title, :url
 end
