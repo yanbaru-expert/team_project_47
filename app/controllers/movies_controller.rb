d3e1deecc8fc6d5b7673ed4770e5de9f37160849
乾　郁絵 <>
2021-04-16 14:39:12 +0900

    ページネーションにbootstrapを適用し、中央へ配置

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc)
-    @movies = Movie.page(params[:page]).per(9)
+    @movies = Movie.page(params[:page]).per(18)
   end
 end
