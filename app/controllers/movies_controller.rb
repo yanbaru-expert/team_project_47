c22d0ad344255a4a8361a58973cd6a6a4513e82b
乾　郁絵 <>
2021-04-16 16:50:23 +0900

    動画の絞り込みと並び替えが反映されるよう修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc)
-    @movies = Movie.page(params[:page]).per(18)
+    @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page]).per(9)
   end
 end
