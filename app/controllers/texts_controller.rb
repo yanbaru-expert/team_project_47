00eda6b2b9a9a4f5b055e88ad3a3701fbf8a0427
ryu8825 <ryuyasoc828@gmail.com>
2021-04-18 08:14:00 +0900

    テキストのジャンル指定

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -2,5 +2,6 @@ class TextsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @texts = Text.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc)
   end
 end
