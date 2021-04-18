44df6eab067102b904e3ce4d44513278fd0d907b
Megurin <tutimur_0521@yahoo.co.jp>
2021-04-18 14:06:03 +0900

    Likeモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_13_090105) do
+ActiveRecord::Schema.define(version: 2021_04_18_045737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 2021_04_13_090105) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "movie_id", null: false
+    t.bigint "text_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["movie_id"], name: "index_likes_on_movie_id"
+    t.index ["text_id"], name: "index_likes_on_text_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "movies", force: :cascade do |t|
     t.integer "genre"
     t.string "title"
@@ -69,4 +80,7 @@ ActiveRecord::Schema.define(version: 2021_04_13_090105) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "movies"
+  add_foreign_key "likes", "texts"
+  add_foreign_key "likes", "users"
 end
