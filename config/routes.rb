cbc8f9bbbe17712e883a811f10982369ef6ce6bd
ryu8825 <ryuyasoc828@gmail.com>
2021-04-20 07:16:07 +0900

    ルーティングを必要なもののみに絞る

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
   root "texts#index"
-  resources :texts
-  resources :movies
+  resources :texts, only: [:index, :show]
+  resources :movies, only: [:index]
 end
