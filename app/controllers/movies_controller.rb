4e275091cf0492b1a6f91e3cf7db8a7f720c4eb2
KENTA MITANI <mitani.kenta88@gmail.com>
2021-05-11 18:08:22 +0900

    動画教材に検索機能実装

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page]).per(9)
+    @q = Movie.ransack(params[:q])
+    @movies = @q.result.page(params[:page]).per(9)
   end
 end
--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,7 +1,8 @@
 class TextsController < ApplicationController
   def index
+    #@texts = Text.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc)
     @q = Text.ransack(params[:q])
-    @texts = @q.result(distinct: true)
+    @texts = @q.result(distinct: true).page(params[:page])
   end
 
   def show
