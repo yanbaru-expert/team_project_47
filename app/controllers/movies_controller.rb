2e49f7453a8589c1938632c27905ed89f452a426
KENTA MITANI <mitani.kenta88@gmail.com>
2021-05-12 17:52:03 +0900

    moviesコントローラ ページネーション修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    genre_movies = Movie.where(genre: ["basic", "git", "ruby", "rails"]).order(id: :asc).page(params[:page]).per(9)
-    @q = genre_movies.ransack(params[:q])
-    @movies = @q.result.page(params[:page]).per(9)
+    @q = Movie.where(genre: ["basic", "git", "ruby", "rails"]).ransack(params[:q])
+    @movies = @q.result.order(id: :asc).page(params[:page]).per(9)
   end
 end
